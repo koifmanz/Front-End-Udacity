8f0ca74077b562189088ad66cc27966c4242362d
koifmanz <zivkaufman@gmail.com>
2022-05-28 16:02:20 +0300

    1st project passed, before sending 2nd project

---
+++ project2/js/app.js
@@ -0,0 +1,106 @@
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
+
+/**
+ * Define Global Variables
+*/
+
+const navBar = document.getElementsByClassName("navbar_menu")[0];
+const sections = document.querySelectorAll("section");
+
+/**
+ * End Global Variables
+ */
+
+/**
+ * Begin Main Functions
+*/
+
+function buildNavBar() {
+
+    // create the ul element to hold the nav items
+    //should be the same as: <ul class="navbar_menu_list navbar__menu"></ul>
+    const nav_area_list = document.createElement("ul");
+    nav_area_list.classList.add("navbar_menu_list");
+    nav_area_list.classList.add("navbar__menu");
+    navBar.appendChild(nav_area_list);
+
+    // the nav bar is built from the sections array
+    // the array is the sections of the site
+    for (let i = 0; i < sections.length; i++) {
+        // loop through the sections array and create a nav bar item for each section
+        const section = sections[i];
+        const sectionId = section.getAttribute("id");
+        const navItem = document.createElement("li");
+        const linkItem = document.createElement('a');
+
+        // update class names for li and a
+        linkItem.innerText = sections[i].querySelector('h2').textContent;
+
+        linkItem.setAttribute("href", "#" + sectionId);
+        linkItem.setAttribute("class", "menu__link");
+        navItem.classList.add("sections[i].id");
+        navItem.classList.add("navbar__menu");
+
+        // add the nav item to the nav bar with the title text and attributes to style
+        navItem.appendChild(linkItem);
+        nav_area_list.appendChild(navItem);
+    };
+}
+
+
+// check if section in in viewport
+function sectionInViewPort() {
+    for (const section of sections) {
+        let rect = section.getBoundingClientRect();
+        const in_viewpiort = rect.top >= 0 && rect.left >= 0
+            && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+
+        if (in_viewpiort) {
+            section.classList.add("active");
+        } else {
+            section.classList.remove("active");
+        }
+    }
+}
+
+/**
+ * End Main Functions
+ */
+
+/**
+ * Begin Events
+*/
+
+// build the nav bar
+buildNavBar();
+
+// update scroll behavior to smooth scroll to section
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// if section in viewport and update the section class
+document.addEventListener('scroll', sectionInViewPort);
+
+/**
+ * End Events
+*/
---
+++ project2/readme.md
@@ -0,0 +1,54 @@
+# Landing Page - Udacity
+
+
+<!-- Describe your project in brief -->
+
+This is project number 2 for Udacity Front-End Nanodegree. The project give me, the student, a real world scenario of manipulating  the DOM, by creating menu based on unknown number of sections. 
+
+
+# Demo-Preview
+
+
+![Alt Text](demo_proj.gif)
+
+
+# Table of contents
+
+- [Landing Page - Udacity](#landing-page---udacity)
+- [Demo-Preview](#demo-preview)
+- [Table of contents](#table-of-contents)
+- [Installation](#installation)
+- [Development](#development)
+- [Footer](#footer)
+
+# Installation
+[(Back to top)](#table-of-contents)
+
+1. download the zip and extract the file.
+2. fork and use git clone
+
+```git clone https://github.com/navendu-pottekkat/nsfw-filter.git``` -->
+
+
+# Development
+[(Back to top)](#table-of-contents)
+
+I used the following languages:
+1. HTML
+2. CSS
+3. JavaScript
+
+You don't need any unique tool to edit except notepad (or any IDE) and a modern updated browser. 
+
+
+
+
+# Footer
+[(Back to top)](#table-of-contents)
+
+<!-- Let's also add a footer because I love footers and also you **can** use this to convey important info.
+
+Let's make it an image because by now you have realised that multimedia in images == cool(*please notice the subtle programming joke). -->
+
+You are welcome to leave a star in GitHub or follow me in Linkdin :) 
+
